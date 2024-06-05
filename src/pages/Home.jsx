c7690c7de1b369fade18a4c5dc30331564a612b3
Abel <abel.tamayo@gmail.com>
2024-06-05 23:24:41 +0200

    First commit. All configured.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <h1 className="text-4xl font-bold text-blue-500">Home</h1>
+    </div>
+  )
+}
+
+export default Home
